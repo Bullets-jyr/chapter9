2456936918a22e8e85ffcd443f5d45c840b66653
Bullets-jyr <jyr@bullets.co.kr>
2023-01-12 21:29:36 +0900

    05. 디바이스 이벤트 알림

--- app/src/main/java/kr/co/bullets/chapter9/MediaPlayerService.kt
+++ app/src/main/java/kr/co/bullets/chapter9/MediaPlayerService.kt
@@ -6,12 +6,14 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.drawable.Icon
 import android.media.MediaPlayer
 import android.os.IBinder
 
 class MediaPlayerService : Service() {
     private var mediaPlayer: MediaPlayer? = null
+    private val receiver = LowBatteryReceiver()
 
     override fun onBind(intent: Intent): IBinder? {
         return null
@@ -20,6 +22,7 @@ class MediaPlayerService : Service() {
     override fun onCreate() {
         super.onCreate()
         createNotificationChannel()
+        initReceiver()
 
         val pauseIcon = Icon.createWithResource(baseContext, R.drawable.ic_baseline_pause_24)
         val playIcon = Icon.createWithResource(baseContext, R.drawable.ic_baseline_play_arrow_24)
@@ -77,6 +80,13 @@ class MediaPlayerService : Service() {
         startForeground(100, notification)
     }
 
+    private fun initReceiver() {
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filter)
+    }
+
     private fun createNotificationChannel() {
         val channel =
             NotificationChannel(CHANNEL_ID, "MEDIA_PLAYER", NotificationManager.IMPORTANCE_DEFAULT)
@@ -113,6 +123,7 @@ class MediaPlayerService : Service() {
             release()
         }
         mediaPlayer = null
+        unregisterReceiver(receiver)
         super.onDestroy()
     }
 }
