d210af42069c2e9cee47022b9d1ce21166ab0ac1
Bullets-jyr <jyr@bullets.co.kr>
2023-01-12 15:19:36 +0900

    02. MediaPlayer 로 음원재생 (1)

--- app/src/main/java/kr/co/bullets/chapter9/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/chapter9/MainActivity.kt
@@ -1,11 +1,48 @@
 package kr.co.bullets.chapter9
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.bullets.chapter9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pauseButton.setOnClickListener {
+            mediaPlayerPause()
+        }
+
+        binding.playButton.setOnClickListener {
+            mediaPlayerPlay()
+        }
+
+        binding.stopButton.setOnClickListener {
+            mediaPlayerStop()
+        }
+    }
+
+    private fun mediaPlayerPause() {
+        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerPlay() {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.cheer).apply {
+                isLooping = true
+            }
+        }
+        mediaPlayer?.start()
+    }
+
+    private fun mediaPlayerStop() {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
